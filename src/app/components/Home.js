61a9c36b1f4756a508cf32185318814a3c03ee81
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-20 11:03:11 +0100

    test heroku update

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,7 +2,7 @@ import { h } from 'preact' // eslint-disable-line no-unused-vars
 
 export default () => (
   <div className='Home page'>
-    <h2>Welcome Adventurer!!</h2>
+    <h2>Welcome Adventurer!!!</h2>
     <p>You are <strong><a href="https://www.google.co.uk/search?q=where+I+am+now">here</a></strong>.</p>
   </div>
 )
