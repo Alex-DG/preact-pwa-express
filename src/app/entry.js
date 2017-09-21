b8c0149aa2b056e3d3ba67a4ab6acca7406808bd
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-21 13:44:01 +0100

    test getInstalledRelatedApps()

--- src/app/entry.js
+++ src/app/entry.js
@@ -8,46 +8,27 @@ import ensurePolyfills from './utils/ensurePolyfills'
 
 const app = document.getElementById('app')
 
-console.log('--> entry');
+navigator.getInstalledRelatedApps().then(relatedApps => {
+  console.log('getInstalledRelatedApps');
+  for (let app of relatedApps) {
+    console.log(app);
+    console.log(app.name);
+  }
+});
 
 ensurePolyfills(() => {
 
-  console.log('--> ensurePolyfills');
-
   const store = createStore(window.__STATE__, window.fetch)
-
-  // window.addEventListener('beforeinstallprompt', function(e) {
-  //   console.log('-> beforeinstallprompt Event fired');
-  //   // beforeinstallprompt Event fired
-  //
-  //   // e.userChoice will return a Promise.
-  //   // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
-  //   e.userChoice.then(function(choiceResult) {
-  //
-  //     console.log(choiceResult.outcome);
-  //
-  //     if(choiceResult.outcome == 'dismissed') {
-  //       console.log('[x] User cancelled home screen install');
-  //     }
-  //     else {
-  //       console.log('[v] User added to home screen');
-  //     }
-  //   });
-  // });
-
   window.addEventListener('popstate', (e) => {
     store.dispatch(updateLocation(window.location.pathname + window.location.search))
   })
-
   store.subscribe(() => {
     const url = getUrl(store.getState())
     if (window.location.pathname + window.location.search !== url) {
       window.history.pushState({}, '', url)
     }
   })
-
   store.dispatch(updateLocation(window.location.pathname + window.location.search))
   store.dispatch(fetchPostsIfNeeded())
-
   render(<App store={store} />, app, app.lastChild)
 })
